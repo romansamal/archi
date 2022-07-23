bfaf992bd6d3872554f67becf18b0553418e392c
Roman Samal <roman.samal.dev@gmail.com>
2022-07-23 18:06:20 +0300

    Add serializable abstract class. Should be used to convert objects with different structure to raw bytes serialize() method is used to convert full object Iterator is used to divide object to some parts and convert every part to raw bytes

--- src/serializable.hpp
+++ src/serializable.hpp
@@ -1,6 +1,18 @@
 #pragma once
+#include <vector>
 
 class serializable
 {
+	class Iterator
+	{
+		virtual ~Iterator() = 0;
+		virtual Iterator &begin() = 0;
+		virtual Iterator &end() = 0;
+		
+		virtual std::vector<uint8_t> &operator*() = 0;
+		virtual bool operator==(Iterator &operand) = 0;
+	};
+
 	virtual ~serializable() = 0;
+	virtual std::vector<uint8_t> serialize() = 0;
 };
