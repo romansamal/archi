ce504bdee178fe599463bd864f26ac42a066522d
Roman Samal <roman.samal.dev@gmail.com>
2022-07-23 22:48:43 +0300

    Add test file creation and deletion

--- tests/serializableFile_test.cpp
+++ tests/serializableFile_test.cpp
@@ -1,18 +1,44 @@
 #include <gtest/gtest.h>
 #include "serializableFile.hpp"
+#include <fstream>
 
 namespace
 {
+    namespace constants
+    {
+        constexpr size_t kiloByte = 1024;
+        constexpr size_t megaByte = kiloByte  * 1024;
+        constexpr size_t gigaByte = megaByte  * 1024;
+
+        constexpr size_t fileSize = 1 * megaByte;
+        constexpr std::string_view fileName = "test_file.txt";
+    };
+
     class serializableFileTest : public testing::Test
     {
     protected:
         void SetUp() override
         {
+            std::srand(static_cast<unsigned>(std::time(nullptr)));
+            fileUT.open(constants::fileName.data(), std::ios::binary);
+            
+            std::vector<char> fileContent;
+            fileContent.resize(constants::fileSize);
+            
+            std::generate(fileContent.begin(), fileContent.end(), []() {
+                return std::rand();
+            });
+
+            fileUT.write(fileContent.data(), fileContent.size());
+            fileUT.close();
         }
 
         void TearDown() override
         {
+            std::remove(constants::fileName.data());
         }
+    private:
+        std::ofstream fileUT;
     };
 
     TEST_F(serializableFileTest, testing)
